f88df551366866c8809c7af60d9062f0f00cfb21
FirdausyTama <atama.firdausy@gmail.com>
2023-03-23 10:33:32 +0700

    void procedur input

--- belajar insertion sort/belajar insertion sort.cpp
+++ belajar insertion sort/belajar insertion sort.cpp
@@ -2,4 +2,11 @@
 using namespace std;
 
 int arr[20];											// membuat array dengan panjang data 20
-int n;													// membuat
+int n;													// membuat variable inputan n
+
+void input() {									// procedure input
+	while (true)
+	{
+
+	}
+}
