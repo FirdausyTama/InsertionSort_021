515283f099f7a92b454881f92abdf7b421094046
FirdausyTama <atama.firdausy@gmail.com>
2023-03-23 10:41:49 +0700

    membuat inputan dan dan isinya

--- belajar insertion sort/belajar insertion sort.cpp
+++ belajar insertion sort/belajar insertion sort.cpp
@@ -7,6 +7,16 @@ int n;													// membuat variable inputan n
 void input() {									// procedure input
 	while (true)
 	{
+		cout << "masukan jumlah data pada array : ";	// membuat inputan jumlah elemnt array
+		cin >> n;										// memanggil variable inputan n
 
+		if (n <= 20) {									// membuat kondisi n tidak lebih dari 20
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukan maksimal 20 elemen. \n"; // menampilkan pesan jika data lebih dari 20
+		}
 	}
+
 }
